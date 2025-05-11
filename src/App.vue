bee7ad89e2dcd20624883517f6bdc227420eff55
yookaaa <ekamaulanahidayat@gmail.com>
2025-05-11 17:50:19 +0700

    setup komponen MyButton dengan props, emit, dan slot

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,37 @@
+<template>
+  <div class="container">
+    <h1>Belajar Components, Props, Emits, dan Slots</h1>
+
+    <MyButton 
+      buttonText="Klik Aku!" 
+      @customClick="handleClick"
+    >
+      <template #default>
+        <strong> - Slot: Ini isi dari induk - </strong>
+      </template>
+    </MyButton>
+
+    <p class="message">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import MyButton from './components/MyButton.vue'
+
+export default {
+  components: {
+    MyButton
+  },
+  data() {
+    return {
+      message: 'Belum diklik'
+    }
+  },
+  methods: {
+    handleClick() {
+      this.message = 'Tombol diklik! Kamu keren!'
+    }
+  }
+}
+</script>
 
